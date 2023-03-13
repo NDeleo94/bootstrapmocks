57fc68dd88f1e0c094a208132025cadeba46370c
NDeleo94 <n.deleo94@gmail.com>
2023-03-13 01:07:07 -0300

    Se traduce el mock html Album a JSX

---
+++ src/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Album from "./Pages/Album";
+
+const Main = () => {
+  return (
+    <div>
+      <Album />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Pages/Album.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AlbumFooter from "../components/AlbumFooter";
+import AlbumHeader from "../components/AlbumHeader";
+import AlbumMain from "../components/AlbumMain";
+
+const Album = () => {
+  return (
+    <>
+      <AlbumHeader />
+      <AlbumMain />
+      <AlbumFooter />
+    </>
+  );
+};
+
+export default Album;
