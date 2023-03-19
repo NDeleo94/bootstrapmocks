d037c5708e77863be78b6257fd94e41ddfe0522c
NDeleo94 <n.deleo94@gmail.com>
2023-03-19 00:22:56 -0300

    Refactorizacion de map

--- src/components/Dashboard/DashboardMainTable.jsx
+++ src/components/Dashboard/DashboardMainTable.jsx
@@ -1,440 +1,173 @@
 import React from "react";
 
 const DashboardMainTable = ({ title }) => {
-  const data = {
-    headers: [
-      {
-        id: 1,
-        headerText: "#",
-      },
-      {
-        id: 2,
-        headerText: "Header",
-      },
-      {
-        id: 3,
-        headerText: "Header",
-      },
-      {
-        id: 4,
-        headerText: "Header",
-      },
-      {
-        id: 5,
-        headerText: "Header",
-      },
-    ],
-    rows: [
-      {
-        id: 1,
-        rowData: [
-          {
-            id: 1,
-            info: "1,001",
-          },
-          {
-            id: 2,
-            info: "random",
-          },
-          {
-            id: 3,
-            info: "data",
-          },
-          {
-            id: 4,
-            info: "placeholder",
-          },
-          {
-            id: 5,
-            info: "text",
-          },
-        ],
-      },
-      {
-        id: 2,
-        rowData: [
-          {
-            id: 1,
-            info: "1,002",
-          },
-          {
-            id: 2,
-            info: "placeholder",
-          },
-          {
-            id: 3,
-            info: "irrelevant",
-          },
-          {
-            id: 4,
-            info: "visual",
-          },
-          {
-            id: 5,
-            info: "layout",
-          },
-        ],
-      },
-      {
-        id: 3,
-        rowData: [
-          {
-            id: 1,
-            info: "1,003",
-          },
-          {
-            id: 2,
-            info: "data",
-          },
-          {
-            id: 3,
-            info: "rich",
-          },
-          {
-            id: 4,
-            info: "dashboard",
-          },
-          {
-            id: 5,
-            info: "tabular",
-          },
-        ],
-      },
-      {
-        id: 4,
-        rowData: [
-          {
-            id: 1,
-            info: "1,003",
-          },
-          {
-            id: 2,
-            info: "information",
-          },
-          {
-            id: 3,
-            info: "placeholder",
-          },
-          {
-            id: 4,
-            info: "illustrative",
-          },
-          {
-            id: 5,
-            info: "data",
-          },
-        ],
-      },
-      {
-        id: 5,
-        rowData: [
-          {
-            id: 1,
-            info: "1,004",
-          },
-          {
-            id: 2,
-            info: "text",
-          },
-          {
-            id: 3,
-            info: "random",
-          },
-          {
-            id: 4,
-            info: "layout",
-          },
-          {
-            id: 5,
-            info: "dashboard",
-          },
-        ],
-      },
-      {
-        id: 6,
-        rowData: [
-          {
-            id: 1,
-            info: "1,005",
-          },
-          {
-            id: 2,
-            info: "dashboard",
-          },
-          {
-            id: 3,
-            info: "irrelevant",
-          },
-          {
-            id: 4,
-            info: "text",
-          },
-          {
-            id: 5,
-            info: "placeholder",
-          },
-        ],
-      },
-      {
-        id: 7,
-        rowData: [
-          {
-            id: 1,
-            info: "1,006",
-          },
-          {
-            id: 2,
-            info: "dashboard",
-          },
-          {
-            id: 3,
-            info: "illustrative",
-          },
-          {
-            id: 4,
-            info: "rich",
-          },
-          {
-            id: 5,
-            info: "data",
-          },
-        ],
-      },
-      {
-        id: 8,
-        rowData: [
-          {
-            id: 1,
-            info: "1,007",
-          },
-          {
-            id: 2,
-            info: "placeholder",
-          },
-          {
-            id: 3,
-            info: "tabular",
-          },
-          {
-            id: 4,
-            info: "information",
-          },
-          {
-            id: 5,
-            info: "irrelevant",
-          },
-        ],
-      },
-      {
-        id: 9,
-        rowData: [
-          {
-            id: 1,
-            info: "1,008",
-          },
-          {
-            id: 2,
-            info: "random",
-          },
-          {
-            id: 3,
-            info: "data",
-          },
-          {
-            id: 4,
-            info: "placeholder",
-          },
-          {
-            id: 5,
-            info: "text",
-          },
-        ],
-      },
-      {
-        id: 10,
-        rowData: [
-          {
-            id: 1,
-            info: "1,009",
-          },
-          {
-            id: 2,
-            info: "placeholder",
-          },
-          {
-            id: 3,
-            info: "irrelevant",
-          },
-          {
-            id: 4,
-            info: "visual",
-          },
-          {
-            id: 5,
-            info: "layout",
-          },
-        ],
-      },
-      {
-        id: 11,
-        rowData: [
-          {
-            id: 1,
-            info: "1,010",
-          },
-          {
-            id: 2,
-            info: "data",
-          },
-          {
-            id: 3,
-            info: "rich",
-          },
-          {
-            id: 4,
-            info: "dashboard",
-          },
-          {
-            id: 5,
-            info: "tabular",
-          },
-        ],
-      },
-      {
-        id: 12,
-        rowData: [
-          {
-            id: 1,
-            info: "1,011",
-          },
-          {
-            id: 2,
-            info: "information",
-          },
-          {
-            id: 3,
-            info: "placeholder",
-          },
-          {
-            id: 4,
-            info: "illustrative",
-          },
-          {
-            id: 5,
-            info: "data",
-          },
-        ],
-      },
-      {
-        id: 13,
-        rowData: [
-          {
-            id: 1,
-            info: "1,012",
-          },
-          {
-            id: 2,
-            info: "text",
-          },
-          {
-            id: 3,
-            info: "placeholder",
-          },
-          {
-            id: 4,
-            info: "layout",
-          },
-          {
-            id: 5,
-            info: "dashboard",
-          },
-        ],
-      },
-      {
-        id: 14,
-        rowData: [
-          {
-            id: 1,
-            info: "1,013",
-          },
-          {
-            id: 2,
-            info: "dashboard",
-          },
-          {
-            id: 3,
-            info: "irrelevant",
-          },
-          {
-            id: 4,
-            info: "text",
-          },
-          {
-            id: 5,
-            info: "visual",
-          },
-        ],
-      },
-      {
-        id: 15,
-        rowData: [
-          {
-            id: 1,
-            info: "1,014",
-          },
-          {
-            id: 2,
-            info: "dashboard",
-          },
-          {
-            id: 3,
-            info: "illustrative",
-          },
-          {
-            id: 4,
-            info: "rich",
-          },
-          {
-            id: 5,
-            info: "data",
-          },
-        ],
-      },
-      {
-        id: 16,
-        rowData: [
-          {
-            id: 1,
-            info: "1,015",
-          },
-          {
-            id: 2,
-            info: "random",
-          },
-          {
-            id: 3,
-            info: "tabular",
-          },
-          {
-            id: 4,
-            info: "information",
-          },
-          {
-            id: 5,
-            info: "text",
-          },
-        ],
-      },
-    ],
-  };
+  const headers = [
+    {
+      id: 1,
+      headerText: "#",
+      selector: (row) => row.propA,
+    },
+    {
+      id: 2,
+      headerText: "Header",
+      selector: (row) => row.propB,
+    },
+    {
+      id: 3,
+      headerText: "Header",
+      selector: (row) => row.propC,
+    },
+    {
+      id: 4,
+      headerText: "Header",
+      selector: (row) => row.propD,
+    },
+    {
+      id: 5,
+      headerText: "Header",
+      selector: (row) => row.propE,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      propA: "1,001",
+      propB: "random",
+      propC: "data",
+      propD: "placeholder",
+      propE: "text",
+    },
+    {
+      id: 2,
+      propA: "1,002",
+      propB: "placeholder",
+      propC: "irrelevant",
+      propD: "visual",
+      propE: "layout",
+    },
+    {
+      id: 3,
+      propA: "1,003",
+      propB: "data",
+      propC: "rich",
+      propD: "dashboard",
+      propE: "tabular",
+    },
+    {
+      id: 4,
+      propA: "1,003",
+      propB: "information",
+      propC: "placeholder",
+      propD: "illustrative",
+      propE: "data",
+    },
+    {
+      id: 5,
+      propA: "1,004",
+      propB: "text",
+      propC: "random",
+      propD: "layout",
+      propE: "dashboard",
+    },
+    {
+      id: 6,
+      propA: "1,005",
+      propB: "dashboard",
+      propC: "irrelevant",
+      propD: "text",
+      propE: "placeholder",
+    },
+    {
+      id: 7,
+      propA: "1,006",
+      propB: "dashboard",
+      propC: "illustrative",
+      propD: "rich",
+      propE: "data",
+    },
+    {
+      id: 8,
+      propA: "1,007",
+      propB: "placeholder",
+      propC: "tabular",
+      propD: "information",
+      propE: "irrelevant",
+    },
+    {
+      id: 9,
+      propA: "1,008",
+      propB: "random",
+      propC: "data",
+      propD: "placeholder",
+      propE: "text",
+    },
+    {
+      id: 10,
+      propA: "1,009",
+      propB: "placeholder",
+      propC: "irrelevant",
+      propD: "visual",
+      propE: "layout",
+    },
+    {
+      id: 11,
+      propA: "1,010",
+      propB: "data",
+      propC: "rich",
+      propD: "dashboard",
+      propE: "tabular",
+    },
+    {
+      id: 12,
+      propA: "1,011",
+      propB: "information",
+      propC: "placeholder",
+      propD: "illustrative",
+      propE: "data",
+    },
+    {
+      id: 13,
+      propA: "1,012",
+      propB: "text",
+      propC: "placeholder",
+      propD: "layout",
+      propE: "dashboard",
+    },
+    {
+      id: 14,
+      propA: "1,013",
+      propB: "dashboard",
+      propC: "irrelevant",
+      propD: "text",
+      propE: "visual",
+    },
+    {
+      id: 15,
+      propA: "1,014",
+      propB: "dashboard",
+      propC: "illustrative",
+      propD: "rich",
+      propE: "data",
+    },
+    {
+      id: 16,
+      propA: "1,015",
+      propB: "random",
+      propC: "tabular",
+      propD: "information",
+      propE: "text",
+    },
+  ];
+
   return (
     <>
-      <h2>{title}</h2>
+      {title ? <h2>{title}</h2> : ""}
       <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
             <tr>
-              {data.headers.map((header) => (
+              {headers.map((header) => (
                 <th scope="col" key={header.id}>
                   {header.headerText}
                 </th>
@@ -442,10 +175,10 @@ const DashboardMainTable = ({ title }) => {
             </tr>
           </thead>
           <tbody>
-            {data.rows.map((row) => (
-              <tr key={row.id}>
-                {row.rowData?.map((d) => (
-                  <td key={d.id}>{d.info}</td>
+            {rows.map((r) => (
+              <tr key={r.id}>
+                {headers.map((h) => (
+                  <td key={h.id}>{h.selector(r)}</td>
                 ))}
               </tr>
             ))}
